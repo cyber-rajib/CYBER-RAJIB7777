a0ebc0d36e821d9311829e282d430fe1cca4dac2
CYBER-RAJIB <muslimhakcerbd@gmail.com>
2024-08-19 00:17:27 +0600

    Update 27 auto dawnload.js

--- scripts/commands/27 auto dawnload.js
+++ scripts/commands/27 auto dawnload.js
@@ -17,14 +17,14 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldown} = require("nayan-media-downloader")
+  const {alldl} = require("rahad-all-downloader")
   if (body.startsWith("https://")) {
   api.setMessageReaction("📿", event.messageID, (err) => {}, true);
-const data = await alldown(content);
+const data = await alldl(content);
   console.log(data)
-  const {low, high, title} = data.data;
+  const {videoUrl, title} = data.data;
     api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true);
-  const video = (await axios.get(high, {
+  const video = (await axios.get(videoUrl, {
       responseType: "arraybuffer",
     })).data;
     fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
@@ -35,5 +35,7 @@ const data = await alldown(content);
 
         }, event.threadID, event.messageID);
     }
+}
+                                 }    }
 }
 }
