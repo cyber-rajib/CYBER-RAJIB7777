97860907819da74a9b9163072e967a1e8b6f8dfe
CYBER-RAJIB <muslimhakcerbd@gmail.com>
2024-08-23 10:56:19 +0600

    Update removebg.js

--- scripts/commands/removebg.js
+++ scripts/commands/removebg.js
@@ -1,31 +1,21 @@
-module.exports.config = {
-  name: 'remove',
-  version: '1.1.1',
-  permssion: 0,
-  credits: 'Islamick Cyber Chat',
-  prefix: 'awto',
-  description: 'Remove Photo Background',
-  category: 'user',
-  usages: 'rbg ( Reply images or url images )',
-  cooldowns: 2,
-  dependencies: {
-       'form-data': '',
-       'image-downloader': ''
-    }
-};
+
 
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs-extra');
 const path = require('path');
 const {image} = require('image-downloader');
+
+
 module.exports.run = async function({
     api, event, args
 }){
     try {
-        if (event.type !== "message_reply") return api.sendMessage("You must reply to a photo", event.threadID, event.messageID);
-        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("You must reply to a photo", event.threadID, event.messageID);
-        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("This is not an image", event.threadID, event.messageID);
+      var tpk = `╭•┄┅═══❁🌺❁═══┅┄•╮\n🖼️= ｢𝐑𝐄𝐌𝐎𝐕𝐄 𝐈𝐌𝐆｣ =🖼️\n╰•┄┅═══❁🌺❁═══┅┄•╯\n✮🩷𝐁𝐀𝐂𝐊𝐆𝐑𝐎𝐔𝐍𝐃🩷✮\n
+⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`;
+        if (event.type !== "message_reply") return api.sendMessage("｢💬｣=> You reply one img", event.threadID, event.messageID);
+        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("｢💬｣=> Reply Your img", event.threadID, event.messageID);
+        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("｢💬｣=> its not img reply a img", event.threadID, event.messageID);
 
         const content = (event.type == "message_reply") ? event.messageReply.attachments[0].url : args.join(" ");
         const KeyApi = ["yxBXi5jTsbax38KauCMFU4bJ","Rh8giMHqNDFwx3CZRW6iJcoF","ke4vcFaSkRAFju2aB9uzmSge","YFobnEtrch553rL5ydKvLeMS","gsHprNw2huCMjc1Vje1nFhTy","rp7if4rdrzkwtvF1ygyJXkJg","Xj9grNLoTFmULT1jRjU62EWb"]
@@ -50,13 +40,22 @@ module.exports.run = async function({
             .then((response) => {
                 if (response.status != 200) return console.error('Error:', response.status, response.statusText);
                 fs.writeFileSync(inputPath, response.data);
-                return api.sendMessage({ attachment: fs.createReadStream(inputPath) }, event.threadID, () => fs.unlinkSync(inputPath));
+                return api.sendMessage({body:tpk, attachment: fs.createReadStream(inputPath)},event.threadID, () => fs.unlinkSync(inputPath));
             })
             .catch((error) => {
                 return console.error('Request failed:', error);
             });
      } catch (e) {
         console.log(e)
-        return api.sendMessage(`có cái nịt`, event.threadID, event.messageID);
+        return api.sendMessage(`｢👾｣ Api sarvar problem`, event.threadID, event.messageID);
   }
 };
+
+module.exports.config = {
+    name: "remove",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Islamick Cyber Chat",
+    description: "",
+    prefix: true,
+    category: "prefix",
