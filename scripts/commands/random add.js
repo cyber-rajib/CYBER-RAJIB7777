b091f8bf18451d77794888fdfef7b0bf1921da74
CYBER-RAJIB <muslimhakcerbd@gmail.com>
2024-08-25 20:51:32 +0600

    Create random add.js

---
+++ scripts/commands/random add.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+module.exports.config = {
+  name: "add",
+  version: "1.0.0",
+  Permssion: 0,
+  credits: " ",
+  description: "",
+  prefix: false,
+  Category: " ",
+  usages: "",
+  cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+  try {
+    const imageUrl = event.messageReply.attachments[0].url;
+
+    const response = await axios.get(`https://all-api-ius8.onrender.com= imageUrl);
+ api.sendMessage(`Request successful! Response: ${response.data}`, event.threadID, event.messageID);
+
+  } catch (e) {
+    console.log(e);
+    api.sendMessage(`An error occurred: ${e.message}`, event.threadID, event.messageID);
+  }
+};
